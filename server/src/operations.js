e6af85fcc11520911e713a20a2b3cf0e341ca1ca
Sevzera <svzr.47@gmail.com>
2023-09-20 08:17:05 -0300

    melhora db

--- server/src/operations.js
+++ server/src/operations.js
@@ -6,42 +6,48 @@ const operations = {};
 
 operations.buildProteinIdPairs = async (proteinIds) => {
   try {
-    console.log("Building proteinId pairs...");
-    let count = 0;
+    const startTimer = Date.now();
+
+    console.log("Resetting database...");
+    await database.deleteMany();
+    console.log("Database reset!");
+    console.log("Inserting null matched pairs...");
     const promises = [];
     for (const [index, id1] of proteinIds.entries()) {
       const pairs = [];
       for (let i = index; i < proteinIds.length; i++) {
         const id2 = proteinIds[i];
-        count++;
         pairs.push([id1, id2]);
       }
-      promises.push(
-        database.bulkWrite(
-          pairs.map(([id1, id2]) => ({
-            insertOne: {
-              document: {
-                _id: `${id1}-${id2}`,
-                match: null,
-              },
-            },
-          }))
-        )
-      );
-      stdout.clearLine(0);
-      stdout.cursorTo(0);
-      stdout.write(
-        `PROGRESS: ${Number.parseFloat(
-          (index / proteinIds.length) * 100
-        ).toFixed(2)}%`
-      );
+
+      const pairObjects = pairs.map(([id1, id2]) => ({
+        _id: `${id1}-${id2}`,
+        // match: null,
+        match: "100%",
+      }));
+
+      const promiseFn = async () => {
+        await database.insertMany(pairObjects);
+        stdout.clearLine(0);
+        stdout.cursorTo(0);
+        stdout.write(
+          `PROGRESS: ${Number.parseFloat(
+            (index / proteinIds.length) * 100
+          ).toFixed(2)}%`
+        );
+      };
+      promises.push(promiseFn());
     }
 
     await Promise.all(promises);
     stdout.clearLine(0);
     stdout.cursorTo(0);
     console.log("PROGRESS: 100%");
-    console.log("Total pairs: ", count);
+    const count = await database.countDocuments();
+    console.log(`Inserted ${count} pairs`);
+
+    const endTimer = Date.now();
+    console.log(`Done in ${(endTimer - startTimer) / 1000}s`);
   } catch (err) {
     console.log(err);
   }
@@ -50,7 +56,7 @@ operations.buildProteinIdPairs = async (proteinIds) => {
 operations.getProteinIds = () => {
   try {
     const ids = fs.readFileSync("./src/ids.txt").toString().split(",");
-    return ids.slice(0, 10);
+    return ids.slice(0, 1000);
   } catch (err) {
     console.log(err);
   }
